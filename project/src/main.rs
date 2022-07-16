e0424243a2b276033ce598241671a7723ffceebb
JackKrub-638 <thanesphol.lee@student.mahidol.edu>
2022-07-16 12:19:35 +0700

    Normal implementation of BFS in Rust is now complete

--- project/src/main.rs
+++ project/src/main.rs
@@ -35,12 +35,13 @@ fn nbrs(vertex: usize, graph: &HashMap<usize, HashSet<usize>>) -> HashSet<usize>
 }
 
 //bfs will print out the path it traverses the graph
+//return hashmap of parent and distance
 fn bfs(source: usize, graph: HashMap<usize, HashSet<usize>>) -> (HashMap<usize,usize>,HashMap<usize,usize>) {
-    let mut frontier: HashSet<usize> = HashSet::from([source]); 
-    let mut parent: HashMap<usize,usize> = HashMap::from([(source, source)]);
-    let mut visited: HashSet<usize> = HashSet::from([source]); //Visited nodes
-    let mut distance: HashMap<usize, usize> = HashMap::new(); //Hashmap of each vertices and its distances from the source 
-    fn expand(frontier: HashSet<usize>, graph: &HashMap<usize, HashSet<usize>>, parent: HashMap<usize,usize>, visited: HashSet<usize>) -> (HashSet<usize>, HashMap<usize,usize>) {
+    let frontier: HashSet<usize> = HashSet::from([source]); 
+    let parent: HashMap<usize,usize> = HashMap::from([(source, source)]);
+    let visited: HashSet<usize> = HashSet::new(); //Visited nodes
+    let distance: HashMap<usize, usize> = HashMap::new(); //Hashmap of each vertices and its distances from the source 
+    fn expand(frontier: HashSet<usize>, graph: &HashMap<usize, HashSet<usize>>, parent: HashMap<usize,usize>, visited: HashSet<usize>) -> (HashSet<usize>, HashMap<usize,usize>, HashSet<usize>) {
         let mut new_frontier: HashSet<usize> = HashSet::new();
         let mut new_parent: HashMap<usize, usize> = HashMap::new();
         let mut new_visited: HashSet<usize> = HashSet::new();
@@ -59,23 +60,23 @@ fn bfs(source: usize, graph: HashMap<usize, HashSet<usize>>) -> (HashMap<usize,u
         for v in frontier {
             new_frontier.remove(&v);
         }
-        (new_frontier, new_parent)
+        (new_frontier, new_parent, new_visited)
     }
-    panic!("hello");
-    // fn iterate(graph: &HashMap<usize, HashSet<usize>>, frontier: HashSet<usize>, parent: HashMap<usize,usize>, distance: HashMap<usize, usize>, d: usize, visited: HashSet<usize>) -> (HashMap<usize,usize>, HashMap<usize, usize>) {
-    //     if frontier.is_empty() {
-    //         (parent, distance)
-    //     }
-    //     else {
-    //         let (new_frontier, new_parent) = expand(frontier, graph, parent,visited);
-    //         let mut new_distance: HashMap<usize, usize> = HashMap::new();
-    //         for (&key, &val) in distance.iter() {
-    //             new_distance.insert(key, val+1);
-    //         }
-    //         iterate(&graph, new_frontier, new_parent,new_distance, d+1, visited)
-    //     }
-    // }
-    // iterate(&graph, frontier, parent, distance, 0, visited)
+    fn iterate(graph: &HashMap<usize, HashSet<usize>>, frontier: HashSet<usize>, parent: HashMap<usize,usize>, distance: HashMap<usize, usize>, d: usize, visited: HashSet<usize>) -> (HashMap<usize,usize>, HashMap<usize, usize>) {
+        if frontier.is_empty() {
+            (parent, distance)
+        }
+        else {
+            let (new_frontier, new_parent, new_visited) = expand(frontier, graph, parent,visited);
+            let mut new_distance: HashMap<usize, usize> = HashMap::new();
+            new_distance.extend(distance);
+            new_frontier.iter().for_each(|&v| {
+                new_distance.insert(v,d+1);
+            });
+            iterate(&graph, new_frontier, new_parent,new_distance, d+1, new_visited)
+        }
+    }
+    iterate(&graph, frontier, parent, distance, 0, visited)
 }
 
 fn main() {
@@ -89,9 +90,18 @@ fn main() {
     let vertex: usize = 1;
     // println!("Neighbours of Vertex: {} are {:?}", vertex, nbrs(vertex, &graph))
 
-    let mut frontier = HashSet::from([2,3]);
-    let mut parent: HashMap<usize,usize> = HashMap::from([(1,1),(2,1),(3,1)]);
-    let mut visited: HashSet<usize> = HashSet::from([1]);
+    let (parent, distance) = bfs(vertex, graph);
+    println!("Parent Map");
+    for (key, value) in parent.iter() {
+        println!("{} -> {}", key, value);
+    }
+    println!("Distance Map");
+    for (keys, values) in distance.iter() {
+        println!("{} -> {}", keys, values);
+    }
+    // let mut frontier = HashSet::from([2,3]);
+    // let mut parent: HashMap<usize,usize> = HashMap::from([(1,1),(2,1),(3,1)]);
+    // let mut visited: HashSet<usize> = HashSet::from([1]);
     // let (new_frontier, new_parent) = expand(frontier,&graph,parent,visited);
     // println!("{:?}",new_frontier);
     // for (key, value) in new_parent.iter() {
