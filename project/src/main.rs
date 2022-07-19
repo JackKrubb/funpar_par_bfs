aea61d431a1ee80230dedde5ea08c14676c697cd
JackKrub-638 <thanesphol.lee@student.mahidol.edu>
2022-07-19 14:27:19 +0700

    Testing new data structure: DashMap

--- project/src/main.rs
+++ project/src/main.rs
@@ -1,4 +1,6 @@
 use std::{collections::{HashMap, HashSet}};
+
+use rayon::prelude::*;
 //use flurry::{HashMap, HashSet};
 
 //input a vertex and return all its neighbours
@@ -6,37 +8,24 @@ fn nbrs(vertex: usize, graph: &HashMap<usize, HashSet<usize>>) -> HashSet<usize>
     graph.get(&vertex).unwrap().clone()
 }
 
-//bfs will print out the path it traverses the graph
-//return hashmap of parent and distance
+//bfs return hashmap of parent and distance
 // fn bfs(source: usize, graph: HashMap<usize, HashSet<usize>>) -> (HashMap<usize,usize>,HashMap<usize,usize>) {
-//     use flurry::{HashMap, HashSet};
-//     //let frontier: HashSet<usize> = HashSet::from([source]); 
-//     let frontier: HashSet<usize> = HashSet::new();
-//     let f_guard = frontier.guard();
-//     frontier.insert(source, &f_guard);
-
-//     //let parent: HashMap<usize,usize> = HashMap::from([(source, source)]);
-//     let parent: HashMap<usize,usize> = HashMap::new();
-//     let p_guard = parent.guard();
-//     parent.pin().insert(source,source);
-
+//     let frontier: HashSet<usize> = HashSet::from([source]); 
+//     let parent: HashMap<usize,usize> = HashMap::from([(source, source)]);
 //     let visited: HashSet<usize> = HashSet::new(); //Visited nodes
-//     let v_guard = visited.guard();
-
 //     let distance: HashMap<usize, usize> = HashMap::new(); //Hashmap of each vertices and its distances from the source 
-//     let d_guard = distance.guard();
-
 //     fn expand(frontier: HashSet<usize>, graph: &HashMap<usize, HashSet<usize>>, parent: HashMap<usize,usize>, visited: HashSet<usize>) -> (HashSet<usize>, HashMap<usize,usize>, HashSet<usize>) {
-//         let mut new_frontier: HashSet<usize> = HashSet::new();
 
-//         let mut new_parent: HashMap<usize, usize> = parent.clone();
-//         let v_guard = visited.guard();
-//         let mut new_visited: HashSet<usize> = visited.iter(&v_guard).cloned().collect();
-        
-        
+//         use dashmap::DashSet;
+//         use dashmap::DashMap;
 
+//         let mut new_frontier = DashSet::new();
+//         let mut new_visited = DashSet::new();
+//         let mut new_parent = DashMap::new();
+//         new_visited.extend(visited);
 //         new_parent.extend(parent);
-//         frontier.iter().for_each(|&v| {
+
+//         frontier.par_iter().map(|&v| {
 //             let neighbours = nbrs(v, graph);
 //             new_visited.insert(v);
 //             for n in neighbours {
@@ -46,9 +35,13 @@ fn nbrs(vertex: usize, graph: &HashMap<usize, HashSet<usize>>) -> HashSet<usize>
 //                 }
 //             }
 //         });
+
 //         for v in frontier {
 //             new_frontier.remove(&v);
 //         }
+
+//         let n_frontier: HashSet<usize> = HashSet::new();
+        
 //         (new_frontier, new_parent, new_visited)
 //     }
 //     fn iterate(graph: &HashMap<usize, HashSet<usize>>, frontier: HashSet<usize>, parent: HashMap<usize,usize>, distance: HashMap<usize, usize>, d: usize, visited: HashSet<usize>) -> (HashMap<usize,usize>, HashMap<usize, usize>) {
@@ -67,22 +60,21 @@ fn nbrs(vertex: usize, graph: &HashMap<usize, HashSet<usize>>) -> HashSet<usize>
 //     }
 //     iterate(&graph, frontier, parent, distance, 0, visited)
 // }
-
 fn main() {
-    // let mut graph: HashMap<usize, HashSet<usize>> = HashMap::new();
-    // graph.insert(1, HashSet::from([2,3]));
-    // graph.insert(2, HashSet::from([1]));
-    // graph.insert(3, HashSet::from([1,4]));
-    // graph.insert(4, HashSet::from([3]));
+    let mut graph: HashMap<usize, HashSet<usize>> = HashMap::new();
+    graph.insert(1, HashSet::from([2,3]));
+    graph.insert(2, HashSet::from([1]));
+    graph.insert(3, HashSet::from([1,4]));
+    graph.insert(4, HashSet::from([3]));
 
 
-    // let mut graph1: HashMap<usize, HashSet<usize>> = HashMap::new();
-    // graph1.insert(1,HashSet::from([2,3]));
-    // graph1.insert(2,HashSet::from([1,5]));
-    // graph1.insert(3,HashSet::from([1,4,5]));
-    // graph1.insert(4, HashSet::from([3,6]));
-    // graph1.insert(5, HashSet::from([2,3]));
-    // graph1.insert(6, HashSet::from([4]));
+    let mut graph1: HashMap<usize, HashSet<usize>> = HashMap::new();
+    graph1.insert(1,HashSet::from([2,3]));
+    graph1.insert(2,HashSet::from([1,5]));
+    graph1.insert(3,HashSet::from([1,4,5]));
+    graph1.insert(4, HashSet::from([3,6]));
+    graph1.insert(5, HashSet::from([2,3]));
+    graph1.insert(6, HashSet::from([4]));
 
     let vertex: usize = 1;
     // println!("Neighbours of Vertex: {} are {:?}", vertex, nbrs(vertex, &graph))
@@ -92,40 +84,45 @@ fn main() {
 
     // // Code block to measure.
     // {
-    //     let (parent, distance) = bfs(vertex, graph1);
-    //     println!("Parent Map");
-    //     for (key, value) in parent.iter() {
-    //         println!("{} -> {}", key, value);
-    //     }
-    //     println!("Distance Map");
-    //     for (keys, values) in distance.iter() {
-    //         println!("{} -> {}", keys, values);
-    //     }
+    //     let (parent, distance) = bfs(vertex, graph);
+    //     // println!("Parent Map");
+    //     // for (key, value) in parent.iter() {
+    //     //     println!("{} -> {}", key, value);
+    //     // }
+    //     // println!("Distance Map");
+    //     // for (keys, values) in distance.iter() {
+    //     //     println!("{} -> {}", keys, values);
+    //     // }
     // }
 
     // let elapsed = now.elapsed();
     // println!("Elapsed: {:.2?}", elapsed);
 
-    use flurry::{HashMap, HashSet};
-    let mut frontier: HashSet<usize> = HashSet::new();
-    let f_guard = frontier.guard();
-    frontier.insert(2, &f_guard);
-    let new_frontier: HashSet<usize> = frontier.iter(&f_guard).cloned().collect();
+    use dashmap::DashSet;
+    use dashmap::DashMap;
 
-    let mut parent: HashMap<usize,usize> = HashMap::new();
-    let p_guard = parent.guard();
-    parent.insert(2, 1, &p_guard);
-    let new_parent: HashMap<usize,usize> = parent.iter(&p_guard).collect();
-    let new_p_guard = new_parent.guard();
-    //let (new_frontier, new_parent) = expand(frontier,&graph,parent,visited);
-    println!("HashSet");
+    let mut frontier: HashSet<usize> = HashSet::new();
+    frontier.insert(1);
+    frontier.insert(2);
+    let mut new_frontier: DashSet<usize> = DashSet::new();
+    new_frontier.extend(frontier.clone());
+
     println!("{:?}",frontier);
     println!("{:?}", new_frontier);
-    println!("HashMap");
-    for (key, value) in parent.iter(&p_guard) {
-        println!("{} -> {}",key, value);
+
+    let mut parent: HashMap<usize,usize> = HashMap::new();
+    parent.insert(1,1);
+    parent.insert(2,2);
+
+    let mut new_parent: DashMap<usize,usize> = DashMap::new();
+    new_parent.extend(parent.clone());
+
+    for (&key, &value) in parent.iter() {
+        println!("{} -> {}", key, value);
     }
-    for (key, value) in new_parent.iter(&new_p_guard) {
-        println!("{} -> {}",key, value);
-    }
+
+    new_parent.iter().for_each(|n| {
+        println!("{} -> {}", n.key(), n.value());
+    })
+    
 }
