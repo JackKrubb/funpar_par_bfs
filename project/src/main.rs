5008b2236762c8d6b1cd4c40e0ed82b1a8698c4c
JackKrub-638 <thanesphol.lee@student.mahidol.edu>
2022-07-19 14:47:03 +0700

    par_bfs is now parallel and working correctly

--- project/src/main.rs
+++ project/src/main.rs
@@ -1,4 +1,6 @@
 use std::{collections::{HashMap, HashSet}};
+use dashmap::DashSet;
+use dashmap::DashMap;
 
 use rayon::prelude::*;
 //use flurry::{HashMap, HashSet};
@@ -7,59 +9,110 @@ use rayon::prelude::*;
 fn nbrs(vertex: usize, graph: &HashMap<usize, HashSet<usize>>) -> HashSet<usize> {
     graph.get(&vertex).unwrap().clone()
 }
+fn bfs(source: usize, graph: HashMap<usize, HashSet<usize>>) -> (HashMap<usize,usize>,HashMap<usize,usize>) {
+    let frontier: HashSet<usize> = HashSet::from([source]); 
+    let parent: HashMap<usize,usize> = HashMap::from([(source, source)]);
+    let visited: HashSet<usize> = HashSet::new(); //Visited nodes
+    let distance: HashMap<usize, usize> = HashMap::new(); //Hashmap of each vertices and its distances from the source 
+    fn expand(frontier: HashSet<usize>, graph: &HashMap<usize, HashSet<usize>>, parent: HashMap<usize,usize>, visited: HashSet<usize>) -> (HashSet<usize>, HashMap<usize,usize>, HashSet<usize>) {
+        let mut new_frontier: HashSet<usize> = HashSet::new();
+        let mut new_parent: HashMap<usize, usize> = HashMap::new();
+        let mut new_visited: HashSet<usize> = HashSet::new();
+
+        new_visited.extend(visited);
+        new_parent.extend(parent);
+        frontier.iter().for_each(|&v| {
+            let neighbours = nbrs(v, graph);
+            new_visited.insert(v);
+            for n in neighbours {
+                if !new_visited.contains(&n) {
+                    new_parent.insert(n, v);
+                    new_frontier.insert(n);
+                }
+            }
+        });
+        for v in frontier {
+            new_frontier.remove(&v);
+        }
+        (new_frontier, new_parent, new_visited)
+    }
+    fn iterate(graph: &HashMap<usize, HashSet<usize>>, frontier: HashSet<usize>, parent: HashMap<usize,usize>, distance: HashMap<usize, usize>, d: usize, visited: HashSet<usize>) -> (HashMap<usize,usize>, HashMap<usize, usize>) {
+        if frontier.is_empty() {
+            (parent, distance)
+        }
+        else {
+            let (new_frontier, new_parent, new_visited) = expand(frontier, graph, parent,visited);
+            let mut new_distance: HashMap<usize, usize> = HashMap::new();
+            new_distance.extend(distance);
+            new_frontier.iter().for_each(|&v| {
+                new_distance.insert(v,d+1);
+            });
+            iterate(&graph, new_frontier, new_parent,new_distance, d+1, new_visited)
+        }
+    }
+    iterate(&graph, frontier, parent, distance, 0, visited)
+}
 
 //bfs return hashmap of parent and distance
-// fn bfs(source: usize, graph: HashMap<usize, HashSet<usize>>) -> (HashMap<usize,usize>,HashMap<usize,usize>) {
-//     let frontier: HashSet<usize> = HashSet::from([source]); 
-//     let parent: HashMap<usize,usize> = HashMap::from([(source, source)]);
-//     let visited: HashSet<usize> = HashSet::new(); //Visited nodes
-//     let distance: HashMap<usize, usize> = HashMap::new(); //Hashmap of each vertices and its distances from the source 
-//     fn expand(frontier: HashSet<usize>, graph: &HashMap<usize, HashSet<usize>>, parent: HashMap<usize,usize>, visited: HashSet<usize>) -> (HashSet<usize>, HashMap<usize,usize>, HashSet<usize>) {
+fn par_bfs(source: usize, graph: HashMap<usize, HashSet<usize>>) -> (HashMap<usize,usize>,HashMap<usize,usize>) {
+    let frontier: HashSet<usize> = HashSet::from([source]); 
+    let parent: HashMap<usize,usize> = HashMap::from([(source, source)]);
+    let visited: HashSet<usize> = HashSet::new(); //Visited nodes
+    let distance: HashMap<usize, usize> = HashMap::new(); //Hashmap of each vertices and its distances from the source 
+    fn expand(frontier: HashSet<usize>, graph: &HashMap<usize, HashSet<usize>>, parent: HashMap<usize,usize>, visited: HashSet<usize>) -> (HashSet<usize>, HashMap<usize,usize>, HashSet<usize>) {
 
-//         use dashmap::DashSet;
-//         use dashmap::DashMap;
+        let mut new_frontier = DashSet::new();
+        let mut new_visited = DashSet::new();
+        let mut new_parent = DashMap::new();
+        new_visited.extend(visited);
+        new_parent.extend(parent);
 
-//         let mut new_frontier = DashSet::new();
-//         let mut new_visited = DashSet::new();
-//         let mut new_parent = DashMap::new();
-//         new_visited.extend(visited);
-//         new_parent.extend(parent);
+        frontier.par_iter().for_each(|&v| {
+            let neighbours = nbrs(v, graph);
+            new_visited.insert(v);
+            for n in neighbours {
+                if !new_visited.contains(&n) {
+                    new_parent.insert(n, v);
+                    new_frontier.insert(n);
+                }
+            }
+        });
 
-//         frontier.par_iter().map(|&v| {
-//             let neighbours = nbrs(v, graph);
-//             new_visited.insert(v);
-//             for n in neighbours {
-//                 if !new_visited.contains(&n) {
-//                     new_parent.insert(n, v);
-//                     new_frontier.insert(n);
-//                 }
-//             }
-//         });
+        for v in frontier {
+            new_frontier.remove(&v);
+        }
 
-//         for v in frontier {
-//             new_frontier.remove(&v);
-//         }
-
-//         let n_frontier: HashSet<usize> = HashSet::new();
+        let mut n_frontier: HashSet<usize> = HashSet::new();
+        for n in new_frontier.iter() {
+            n_frontier.insert(*n.key());
+        }
+        let mut n_parent: HashMap<usize,usize> = HashMap::new();
+        for n in new_parent.iter() {
+            n_parent.insert(*n.key(), *n.value());
+        }
+        let mut n_visited: HashSet<usize> = HashSet::new();
+        for n in new_visited.iter() {
+            n_visited.insert(*n.key());
+        }
         
-//         (new_frontier, new_parent, new_visited)
-//     }
-//     fn iterate(graph: &HashMap<usize, HashSet<usize>>, frontier: HashSet<usize>, parent: HashMap<usize,usize>, distance: HashMap<usize, usize>, d: usize, visited: HashSet<usize>) -> (HashMap<usize,usize>, HashMap<usize, usize>) {
-//         if frontier.is_empty() {
-//             (parent, distance)
-//         }
-//         else {
-//             let (new_frontier, new_parent, new_visited) = expand(frontier, graph, parent,visited);
-//             let mut new_distance: HashMap<usize, usize> = HashMap::new();
-//             new_distance.extend(distance);
-//             new_frontier.iter().for_each(|&v| {
-//                 new_distance.insert(v,d+1);
-//             });
-//             iterate(&graph, new_frontier, new_parent,new_distance, d+1, new_visited)
-//         }
-//     }
-//     iterate(&graph, frontier, parent, distance, 0, visited)
-// }
+        (n_frontier, n_parent, n_visited)
+    }
+    fn iterate(graph: &HashMap<usize, HashSet<usize>>, frontier: HashSet<usize>, parent: HashMap<usize,usize>, distance: HashMap<usize, usize>, d: usize, visited: HashSet<usize>) -> (HashMap<usize,usize>, HashMap<usize, usize>) {
+        if frontier.is_empty() {
+            (parent, distance)
+        }
+        else {
+            let (new_frontier, new_parent, new_visited) = expand(frontier, graph, parent,visited);
+            let mut new_distance: HashMap<usize, usize> = HashMap::new();
+            new_distance.extend(distance);
+            new_frontier.iter().for_each(|&v| {
+                new_distance.insert(v,d+1);
+            });
+            iterate(&graph, new_frontier, new_parent,new_distance, d+1, new_visited)
+        }
+    }
+    iterate(&graph, frontier, parent, distance, 0, visited)
+}
 fn main() {
     let mut graph: HashMap<usize, HashSet<usize>> = HashMap::new();
     graph.insert(1, HashSet::from([2,3]));
@@ -79,66 +132,42 @@ fn main() {
     let vertex: usize = 1;
     // println!("Neighbours of Vertex: {} are {:?}", vertex, nbrs(vertex, &graph))
 
-    // use std::time::Instant;
-    // let now = Instant::now();
+    use std::time::Instant;
+    let now = Instant::now();
 
-    // // Code block to measure.
-    // {
-    //     let (parent, distance) = bfs(vertex, graph);
-    //     // println!("Parent Map");
-    //     // for (key, value) in parent.iter() {
-    //     //     println!("{} -> {}", key, value);
-    //     // }
-    //     // println!("Distance Map");
-    //     // for (keys, values) in distance.iter() {
-    //     //     println!("{} -> {}", keys, values);
-    //     // }
-    // }
-
-    // let elapsed = now.elapsed();
-    // println!("Elapsed: {:.2?}", elapsed);
-
-    use dashmap::DashSet;
-    use dashmap::DashMap;
-
-    let mut frontier: HashSet<usize> = HashSet::new();
-    frontier.insert(1);
-    frontier.insert(2);
-    let mut new_frontier: DashSet<usize> = DashSet::new();
-    new_frontier.extend(frontier.clone());
-
-    let mut n_frontier: HashSet<usize> = HashSet::new();
-    for n in new_frontier.iter() {
-        n_frontier.insert(*n.key());
+    // Code block to measure.
+    {
+        let (parent, distance) = bfs(vertex, graph1.clone());
+        println!("Parent Map");
+        for (key, value) in parent.iter() {
+            println!("{} -> {}", key, value);
+        }
+        println!("Distance Map");
+        for (keys, values) in distance.iter() {
+            println!("{} -> {}", keys, values);
+        }
     }
 
-    println!("{:?}",frontier);
-    println!("{:?}", new_frontier);
-    println!("{:?}",n_frontier);
+    let elapsed = now.elapsed();
+    println!("normal_bfs Elapsed: {:.2?}", elapsed);
 
-    let mut parent: HashMap<usize,usize> = HashMap::new();
-    parent.insert(1,1);
-    parent.insert(2,2);
+    let now = Instant::now();
 
-    let mut new_parent: DashMap<usize,usize> = DashMap::new();
-    new_parent.extend(parent.clone());
-
-    let mut n_parent: HashMap<usize,usize> = HashMap::new();
-    for n in new_parent.iter() {
-        n_parent.insert(*n.key(), *n.value());
+    // Code block to measure.
+    {
+        let (parent, distance) = par_bfs(vertex, graph1.clone());
+        println!("Parent Map");
+        for (key, value) in parent.iter() {
+            println!("{} -> {}", key, value);
+        }
+        println!("Distance Map");
+        for (keys, values) in distance.iter() {
+            println!("{} -> {}", keys, values);
+        }
     }
 
-    for (&key, &value) in parent.iter() {
-        println!("{} -> {}", key, value);
-    }
-
-    new_parent.iter().for_each(|n| {
-        println!("{} -> {}", n.key(), n.value());
-    });
-
-    for (&key, &value) in n_parent.iter() {
-        println!("{} -> {}", key, value);
-    }
+    let elapsed = now.elapsed();
+    println!("par_bfs Elapsed: {:.2?}", elapsed);
 
     
 }
